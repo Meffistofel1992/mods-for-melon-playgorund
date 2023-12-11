0a4d8ca66e206a06874a38fec873e7f0777643cc
Oleksandr Kovalov <o.kovalov@soloweigroup.com>
2023-12-11 22:34:25 +0200

    completed root app file

--- mods-for-melon-playgorund/mods_for_melon_playgorundApp.swift
+++ mods-for-melon-playgorund/mods_for_melon_playgorundApp.swift
@@ -6,13 +6,33 @@
 //
 
 import SwiftUI
+import Resolver
 
 @main
 struct mods_for_melon_playgorundApp: App {
 
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
+    @Injected private var dropBox: Dropbox_MMP
+    @Injected private var coreDataStore: CoreDataStore_MMP
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear { basicSetup() }
+                .environment(\.managedObjectContext, coreDataStore.viewContext)
         }
+    }
+
+    private func basicSetup() {
+        UIApplication.shared.addTapGestureRecognizer()
+        dropBox.initDropBox_MMP()
+
+        UIButton.appearance().isMultipleTouchEnabled = false
+        UIButton.appearance().isExclusiveTouch = true
+        UIView.appearance().isMultipleTouchEnabled = false
+        UIView.appearance().isExclusiveTouch = true
+
+        UIRefreshControl.appearance().tintColor = UIColor.black
     }
 }
